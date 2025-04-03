d1766f8fd9dde33db2888f4aec9563ac965cac6f
adriany5357 <adrianaa@msmk.university>
2025-04-03 10:58:36 +0200

    buscador de paises

---
+++ src/app/pages/paises/paises.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-paises',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './paises.component.html',
+  styleUrl: './paises.component.css'
+})
+export class PaisesComponent {
+
+  paisBuscar: string = "";
+  paisRecibido:any;
+  continente:any;
+  error:any;
+
+  getPaisesWithFetch(pais: string) {
+    fetch(`https://restcountries.com/v3.1/name/${pais}?fullText=false`)
+    //fetch(`https://restcountries.com/v3.1/name/${pais}`)
+      .then(response => response.json())
+      .then(data => {
+        this.paisRecibido = data [0]
+        this.continente=data[0].continents[0]
+        this.error=null
+        console.log('Fetch:', data)
+      })
+      .catch(error =>{
+        this.error=error;
+        this.paisRecibido=null
+        console.error('Error con Fetch:', error);
+      } )
+  }
+
+}
